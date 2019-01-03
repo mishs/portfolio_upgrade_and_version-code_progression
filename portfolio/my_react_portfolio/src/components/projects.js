6be40284b0501cba33a1e945b45dbc1322759c23
Mish Siwela <mishuct26@gmail.com>
2019-01-03 18:28:41 +0200

    React-mdl Tabs for categories

--- portfolio/my_react_portfolio/src/components/projects.js
+++ portfolio/my_react_portfolio/src/components/projects.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import { Tabs, Tab } from 'react-mdl';
 
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return (
+        <div>
+          <h1>This is React</h1>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h1>Projects</h1>
+      <div className="technologies-tabs">
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={tabId => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>React</Tab>
+          <Tab>HTML and CSS3</Tab>
+          <Tab>Bootstrap</Tab>
+          <Tab>JavaScript</Tab>
+          <Tab>Node.js</Tab>
+          <Tab>MongoDB</Tab>
+        </Tabs>
+
+        <section className="project-grid">{this.toggleCategories()}</section>
       </div>
     );
   }
