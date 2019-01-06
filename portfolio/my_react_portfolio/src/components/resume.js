1fe1d8e65dec8fbe5964fd3e38c237901ef7ba87
Mish Siwela <mishuct26@gmail.com>
2019-01-07 01:39:10 +0200

    Left side image in

--- portfolio/my_react_portfolio/src/components/resume.js
+++ portfolio/my_react_portfolio/src/components/resume.js
@@ -6,7 +6,15 @@ class Resume extends Component {
     return (
       <div>
         <Grid>
-          <Cell col={4}>Left Side</Cell>
+          <Cell col={4}>
+            <div style={{ textAlign: 'center' }}>
+              <img
+                src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+                alt="avatar"
+                style={{ height: '200px' }}
+              />
+            </div>
+          </Cell>
           <Cell col={8} className="resume-right-col">
             RS
           </Cell>
