627b463968e7fe20f9db8d25c69d01da9f97f199
Mish Siwela <mishuct26@gmail.com>
2019-01-07 03:55:39 +0200

    Skills and Home Page Link

--- portfolio/my_react_portfolio/src/App.js
+++ portfolio/my_react_portfolio/src/App.js
@@ -9,7 +9,15 @@ class App extends Component {
     return (
       <div className="demo-big-content">
         <Layout>
-          <Header className="header-color" title="Title" scroll>
+          <Header
+            className="header-color"
+            title={
+              <Link style={{ textDecoration: 'none', color: 'white' }} to="/">
+                MyPortfolio
+              </Link>
+            }
+            scroll
+          >
             <Navigation>
               <Link to="/aboutme">About Me</Link>
               <Link to="/contact">Contact</Link>
--- portfolio/my_react_portfolio/src/components/resume.js
+++ portfolio/my_react_portfolio/src/components/resume.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
 import Education from './education';
+import Experience from './experience';
+import Skills from './skills';
 
 class Resume extends Component {
   render() {
@@ -40,12 +42,47 @@ class Resume extends Component {
           </Cell>
           <Cell col={8} className="resume-right-col">
             <h2>Education</h2>
-
             <Education
               startYear={2005}
               endYear={2006}
               schoolName="Advanced Level"
               schoolDescription="Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown "
+            />
+            <Education
+              startYear={2005}
+              endYear={2006}
+              schoolName="University of South Africa- Unisa "
+              schoolDescription="Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown "
+            />
+            <hr style={{ borderTop: '3px solid #e22947' }} />
+            <h2>Experience</h2>
+            <Experience
+              startYear={2018}
+              endYear={2018}
+              jobName="Search Engi Optimiser"
+              jobDescription="hat is Lorem Ipsum
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also th"
+            />
+            <Experience
+              startYear={2018}
+              endYear={2018}
+              jobName="Search Engi Optimiser"
+              jobDescription="hat is Lorem Ipsum
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also th"
+            />
+            <hr style={{ borderTop: '3px solid #e22947' }} />
+            <h2>Skills</h2>
+            <Skills skill="JavaScript" progress={100} />
+            skill="JavaScript" progress={100}
+            />
+            <Skills skill="JavaScript" progress={100} />
+            skill="JavaScript" progress={100}
+            />
+            <Skills skill="JavaScript" progress={100} />
+            skill="JavaScript" progress={90}
+            />
+            <Skills skill="JavaScript" progress={50} />
+            skill="JavaScript" progress={60}
             />
           </Cell>
         </Grid>
